df776316394536375d9aeedc1e0c13cb2a5c89c3
nottiffchan <nottiffchan@gmail.com>
2020-12-13 23:47:02 +0800

    Display and update burger price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,6 +64,7 @@ class BurgerBuilder extends Component {
                 addIngredient={this.addIngredientHandler}
                 removeIngredient={this.removeIngredientHandler}
                 disabled={disabledInfo}
+                price={this.state.totalPrice}
               />
           </Aux>  
         );
